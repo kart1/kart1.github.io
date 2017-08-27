af634110ee9058b23774a1c9f035087d6fe3f671
EthanDoan <doanphuong@Doans-MacBook-Pro-2.local>
2017-08-27 13:51:00 +0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function(){
+// 	$('#map-container').click(function () {
+//     	$('#map-container').css("pointer-events", "auto");
+//     	console.log('clicked');
+// 	});
+
+// 	$( "#map-container" ).mouseleave(function() {
+//   		$('#map-container').css("pointer-events", "none"); 
+//   		console.log('mouse leave');
+// 	});
+// });
+
+
+
+
